fc8f941b4b24c37d4981a81d523f7d82224561e4
perno97 <fabio.perini97@gmail.com>
2017-06-23 15:53:53 +0200

    Google sign in.

--- app/src/main/java/it/unibs/cloudondemand/LoginActivity.java
+++ app/src/main/java/it/unibs/cloudondemand/LoginActivity.java
@@ -3,11 +3,20 @@ package it.unibs.cloudondemand;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
     }
 }
