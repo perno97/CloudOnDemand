d2594bf7b94782de4fec5a63137d436afc717bab
matteo <m.tabarelli@studenti.unibs.it>
2017-06-23 17:20:50 +0200

    Google Sign-in button

--- app/src/main/java/it/unibs/cloudondemand/LoginActivity.java
+++ app/src/main/java/it/unibs/cloudondemand/LoginActivity.java
@@ -3,11 +3,14 @@ package it.unibs.cloudondemand;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.GoogleApiClient;
 
-public class LoginActivity extends AppCompatActivity {
+import it.unibs.cloudondemand.google.LoginGoogle;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +20,17 @@ public class LoginActivity extends AppCompatActivity {
         // Get the Intent that started this activity and extract the string
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        //Sign in when the user clicks on Google sign-in button
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
+        onClick(findViewById(R.id.sign_in_button));
+
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+            Intent signInIntent = new Intent(this, LoginGoogle.class);
+            startActivity(signInIntent);
     }
 }
