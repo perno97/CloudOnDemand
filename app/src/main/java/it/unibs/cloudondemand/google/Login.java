f82f3f80ad771a5be4d9e434fc02a9dd6d8c3250
perno97 <fabio.perini97@gmail.com>
2017-06-23 16:47:15 +0200

    Google sign in.

--- app/src/main/java/it/unibs/cloudondemand/google/Login.java
+++ app/src/main/java/it/unibs/cloudondemand/google/Login.java
@@ -1,9 +1,14 @@
 package it.unibs.cloudondemand.google;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 
 import it.unibs.cloudondemand.LoginInterface;
@@ -13,9 +18,13 @@ import it.unibs.cloudondemand.LoginInterface;
  */
 
 public class Login implements LoginInterface {
+    Context context;
     GoogleApiClient mGoogleApiClient;
     private static final int RC_SIGN_IN = 1;
 
+    public Login(Context context){
+        this.context=context;
+    }
     @Override
     public void doLogin() {
         if(isLogged()) return;
@@ -28,15 +37,25 @@ public class Login implements LoginInterface {
 
         // Build a GoogleApiClient with access to the Google Sign-In API and the
         // options specified by gso.
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+        mGoogleApiClient = new GoogleApiClient.Builder(context)
+                .enableAutoManage((FragmentActivity) context /* FragmentActivity */, (GoogleApiClient.OnConnectionFailedListener)context /* OnConnectionFailedListener */)
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
                 .build();
     }
 
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            Toast.makeText(context, "funziona", Toast.LENGTH_SHORT).show();
+        }
+    }
+
     private void signIn() {
+        Activity a = (Activity)context;
         Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
-        startActivityForResult(signInIntent, RC_SIGN_IN);
+        a.startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
     @Override
