c84c30233431b43d10787b3e8218d96a46f233cd
perno97 <fabio.perini97@gmail.com>
2017-06-23 14:59:44 +0200

    Interfacce.

---
+++ app/src/main/java/it/unibs/cloudondemand/LoginInterface.java
@@ -0,0 +1,11 @@
+package it.unibs.cloudondemand;
+
+/**
+ * Created by Perno on 23/06/2017.
+ */
+
+public interface LoginInterface {
+    void doLogin();
+    boolean isLogged();
+    void doLogout();
+}
