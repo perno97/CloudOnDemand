54f4747b6cec805c9ccab0fe048f3ad1f4fc412d
perno97 <fabio.perini97@gmail.com>
2017-06-23 17:21:30 +0200

    Google sign in.

--- app/src/main/java/it/unibs/cloudondemand/google/LoginGoogle.java
+++ app/src/main/java/it/unibs/cloudondemand/google/LoginGoogle.java
@@ -3,6 +3,8 @@ package it.unibs.cloudondemand.google;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,60 +13,68 @@ import android.widget.Toast;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 
 import it.unibs.cloudondemand.LoginInterface;
 import it.unibs.cloudondemand.R;
 
-public class LoginGoogle extends AppCompatActivity {
+public class LoginGoogle extends AppCompatActivity implements LoginInterface, GoogleApiClient.OnConnectionFailedListener {
+    private GoogleApiClient mGoogleApiClient;
+    private static final int RC_SIGN_IN = 1;
 
-        Context context;
-        GoogleApiClient mGoogleApiClient;
-        private static final int RC_SIGN_IN = 1;
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
 
-        public Login(Context context){
-            this.context=context;
-        }
-        @Override
-        public void doLogin() {
-            if(isLogged()) return;
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
 
-            // Configure sign-in to request the user's ID, email address, and basic
-            // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
-            GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                    .requestEmail()
-                    .build();
+        // Build a GoogleApiClient with access to the Google Sign-In API and the
+        // options specified by gso.
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
 
-            // Build a GoogleApiClient with access to the Google Sign-In API and the
-            // options specified by gso.
-            mGoogleApiClient = new GoogleApiClient.Builder(context)
-                    .enableAutoManage((FragmentActivity) context /* FragmentActivity */, (GoogleApiClient.OnConnectionFailedListener)context /* OnConnectionFailedListener */)
-                    .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-                    .build();
-        }
+        doLogin();
+    }
 
-        public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        // An unresolvable error has occurred and a connection to Google APIs
+        // could not be established. Display an error message, or handle
+        // the failure silently
 
-            // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
-            if (requestCode == RC_SIGN_IN) {
-                GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-                Toast.makeText(context, "funziona", Toast.LENGTH_SHORT).show();
-            }
-        }
+        // ...
+    }
 
-        private void signIn() {
-            Activity a = (Activity)context;
-            Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
-            a.startActivityForResult(signInIntent, RC_SIGN_IN);
-        }
+    @Override
+    public void doLogin() {
+        if (isLogged()) return;
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
 
-        @Override
-        public boolean isLogged() {
-            return false;
-        }
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
 
-        @Override
-        public void doLogout() {
-
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            Toast.makeText(this, "funziona", Toast.LENGTH_SHORT).show();
         }
     }
+
+    @Override
+    public boolean isLogged() {
+        return false;
+    }
+
+    @Override
+    public void doLogout() {
+
+    }
+}
