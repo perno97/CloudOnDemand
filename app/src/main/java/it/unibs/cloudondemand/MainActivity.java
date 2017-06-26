80eff91f6ad17da62b447e730e3a58cd11510c0a
matteo <m.tabarelli@studenti.unibs.it>
2017-06-26 18:50:19 +0200

    ListView quasi fatta

--- app/src/main/java/it/unibs/cloudondemand/MainActivity.java
+++ app/src/main/java/it/unibs/cloudondemand/MainActivity.java
@@ -1,18 +1,39 @@
 package it.unibs.cloudondemand;
 
 import android.content.Intent;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "it.unibs.cloudondemand.MESSAGE";
+    private String path = Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.row, R.id.riga, readFiles(path));
+            ListView listView = (ListView) findViewById(R.id.listview);
+            listView.setAdapter(adapter);
+            }
+        catch (IOException e) {
+            Toast.makeText(this, "Errore lettura file", Toast.LENGTH_SHORT).show();
+        }
     }
 
     /** Called when the user taps the Send button */
@@ -27,7 +48,34 @@ public class MainActivity extends AppCompatActivity {
             intent.putExtra(EXTRA_MESSAGE, message);
             startActivity(intent);
         }
+    }
 
+    /**Read files from external storage*/
+    public static String [] readFiles(String strFile) throws IOException {
+        File file = new File(strFile);
+
+        if(file.exists()) {
+            if (file.isFile()) {
+                String[] fileList=new String[]{file.getName()};
+                return fileList;
+            }
+            else if (file.isDirectory()) {
+                File[] fileList=file.listFiles();
+                String[] fileListName=new String[fileList.length];
+                for (int i = 0; i<=fileList.length;i++)
+                {
+                    fileListName[i]=fileList[i].getName();
+                }
+                return fileListName;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        else
+            throw new IOException();
     }
 }
+
 
