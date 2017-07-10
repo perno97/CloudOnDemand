71dd945c81995a0f4a4c9d0530a6415b1216a317
lanzi97 <lanzi97@hotmail.it>
2017-07-10 16:25:30 +0200

    Path Finder UI funziona

--- app/src/main/java/it/unibs/cloudondemand/MainActivity.java
+++ app/src/main/java/it/unibs/cloudondemand/MainActivity.java
@@ -5,35 +5,67 @@ import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 
+import it.unibs.cloudondemand.utils.PermissionRequest;
+import it.unibs.cloudondemand.utils.PermissionResultCallback;
+import it.unibs.cloudondemand.utils.Utils;
+
 public class MainActivity extends AppCompatActivity {
-    private String path = Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator;
+    private File currentPath = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator);
+    private File[] currentFileList;
+    private final ArrayList<String> currentFileListString = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /*
-        try {
-            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.row, R.id.riga, readFiles(path));
-            ListView listView = (ListView) findViewById(R.id.listview);
-            listView.setAdapter(adapter);
-            }
-        catch (IOException e) {
-            Toast.makeText(this, "Errore lettura file", Toast.LENGTH_SHORT).show();
-        }*/
+
+        // Check if external storage is readable
+        if (Utils.isExternalStorageReadable()) {
+            // Verify permission and after fill listview
+            Intent intent = PermissionRequest.getRequestPermissionIntent(this, android.Manifest.permission.READ_EXTERNAL_STORAGE, permissionResultCallback);
+            startActivity(intent);
+        }
+        else {
+                Toast.makeText(this, "Errore lettura file", Toast.LENGTH_SHORT).show();
+        }
+
     }
+
+    // Called when user chose to grant permission
+    private final PermissionResultCallback permissionResultCallback = new PermissionResultCallback() {
+        @Override
+        public void onPermissionResult(int isGranted) {
+            // Blocks code if permission is denied
+            if(isGranted == PermissionRequest.PERMISSION_DENIED)
+                return;
+
+            // Fill listview
+            ListView listView = (ListView) findViewById(R.id.listview);
+            try {
+                readFiles();
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, R.layout.row, R.id.riga, currentFileListString);
+                listView.setAdapter(adapter);
+            }
+            catch (IOException e) {
+                Toast.makeText(MainActivity.this, "Errore lettura file", Toast.LENGTH_SHORT).show();
+            }
+
+            // Add on item click listener to listview items
+            listView.setOnItemClickListener(onItemClick);
+        }
+    };
 
     /** Called when the user taps the Send button */
     public void sendMessage(View view) {
@@ -50,32 +82,72 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    /**Read files from external storage*/
-    public static String [] readFiles(String strFile) throws IOException {
-        File file = new File(strFile);
-
-        if(file.exists()) {
-            if (file.isFile()) {
+    /**Read files from external storage*/ //TODO gestire IOEXceotion
+    public void readFiles() throws IOException {
+        if(currentPath.exists()) {
+            if (currentPath.isFile()) {
+                // Open popup when clicked on file ... listview doesn't change
+                /*
                 String[] fileList=new String[]{file.getName()};
-                return fileList;
+                return fileList; */
             }
-            else if (file.isDirectory()) {
-                File[] fileList=file.listFiles();
-                String[] fileListName=new String[fileList.length];
-                for (int i = 0; i<=fileList.length;i++)
+            else if (currentPath.isDirectory()) {
+                currentFileList=currentPath.listFiles();
+                // Clear string array
+                currentFileListString.clear();
+                // Add first directory (back) /..
+                currentFileListString.add("/..");
+                // Fill it with name of files
+                for (int i = 0; i<currentFileList.length;i++)
                 {
-                    fileListName[i]=fileList[i].getName();
+                    File temp = currentFileList[i];
+                    if (temp.isDirectory())
+                        currentFileListString.add(currentFileList[i].getName() + "/");
+                    else
+                        currentFileListString.add(currentFileList[i].getName());
                 }
-                return fileListName;
-            }
-            else
-            {
-                return null;
             }
         }
-        else
-            throw new IOException();
+        else {
+            throw new FileNotFoundException();
+        }
     }
+
+    // Callback click su listview item
+    private final AdapterView.OnItemClickListener onItemClick = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            boolean refreshListView = false;
+
+            if (position == 0) {
+                currentPath = currentPath.getParentFile();
+                refreshListView = true;
+            }
+            else {
+                File selected = currentFileList[position-1];
+                if (selected.isDirectory()) {
+                    currentPath = selected;
+                    refreshListView = true;
+                }
+                else if (selected.isFile()) {
+                    Toast.makeText(MainActivity.this, "E' un file", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+
+            if (refreshListView) {
+                //TODO rimuovere try catch
+                try {
+                    // Update array with new path
+                    readFiles();
+                    // Notify update to listview
+                    ((BaseAdapter) parent.getAdapter()).notifyDataSetChanged();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    };
 }
 
 
