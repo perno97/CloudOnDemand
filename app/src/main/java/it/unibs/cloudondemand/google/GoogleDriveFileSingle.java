3ba8a848270dfbdc42cbeb028e9ba0cf1ba317c1
lanzi97 <lanzi97@hotmail.it>
2017-07-11 00:46:07 +0200

    Testing progress bar

--- app/src/main/java/it/unibs/cloudondemand/google/GoogleDriveFileSingle.java
+++ app/src/main/java/it/unibs/cloudondemand/google/GoogleDriveFileSingle.java
@@ -1,7 +1,11 @@
 package it.unibs.cloudondemand.google;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.util.Log;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.api.ResultCallback;
@@ -17,6 +21,8 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.OutputStream;
 
+import it.unibs.cloudondemand.R;
+
 public class GoogleDriveFileSingle extends GoogleDriveFile {
     private static final String TAG = "GoogleDriveUpSingleFile";
 
@@ -25,6 +31,9 @@ public class GoogleDriveFileSingle extends GoogleDriveFile {
         // Start creating new drive content and fill it in callback
         Drive.DriveApi.newDriveContents(getGoogleApiClient())
                 .setResultCallback(driveContentsCallback);
+        /* TESTING */
+        TextView textname = (TextView) findViewById(R.id.upload_textname);
+        textname.setText("File : " + getContent());
     }
 
 
@@ -53,16 +62,20 @@ public class GoogleDriveFileSingle extends GoogleDriveFile {
                         // Open file
                         FileInputStream fileInputStream = new FileInputStream(file);
                         outputStream = driveContents.getOutputStream();
-                        // Write on drive content stream
-                        int buffer;
-                        while ((buffer = fileInputStream.read()) != -1) {
+                        // Write on drive content stream with buffer of 8 bytes
+                        byte[] buffer = new byte[8];
+                        long k = 0;
+                        while (fileInputStream.read(buffer) != -1) {
+                            updateProgressBar(100*k/(file.length()));
                             outputStream.write(buffer);
+                            k+=8;
                         }
                     } catch (FileNotFoundException e) {
                         Log.e(TAG, "File not found." + e.toString(), e.getCause());
                     } catch (IOException e) {
                         Log.e(TAG, "Exception while writing on driveConetents output stream." + e.toString(), e.getCause());
                     } finally {
+                        // Close output stream
                         try {
                             if (outputStream != null)
                                 outputStream.close();
@@ -70,7 +83,6 @@ public class GoogleDriveFileSingle extends GoogleDriveFile {
                             Log.e(TAG, "Exception while closing streams." + e.toString(), e.getCause());
                         }
                     }
-
 
                     MetadataChangeSet changeSet = new MetadataChangeSet.Builder()
                             .setTitle(file.getName())
@@ -95,9 +107,35 @@ public class GoogleDriveFileSingle extends GoogleDriveFile {
             } else {
                 Toast.makeText(GoogleDriveFileSingle.this, "File Creato", Toast.LENGTH_SHORT).show();
                 Log.i(TAG, "File created. " + driveFileResult.getDriveFile().getDriveId());
+                /* TESTING */
+                updateProgressBar(100);
+                TextView textid = (TextView) findViewById(R.id.upload_textid);
+                textid.setText("DriveID : " + driveFileResult.getDriveFile().getDriveId());
             }
 
             disconnect();
         }
     };
+
+    /* TESTING */
+
+    private void updateProgressBar (final long percent) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                ProgressBar progressBar = (ProgressBar) findViewById(R.id.upload_progress_bar);
+                progressBar.setProgress((int) percent);
+                TextView textProtgress = (TextView) findViewById(R.id.upload_textprogress);
+                textProtgress.setText(percent + "%");
+            }
+        });
+    }
+
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload);
+    }
 }
