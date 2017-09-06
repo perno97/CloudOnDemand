2b51a8ae6ebdf7d112cc9943d56ee5f008de1a94
lanzi97 <lanzi97@hotmail.it>
2017-09-06 18:28:37 +0200

    Un bordello. https://codelabs.developers.google.com/codelabs/appauth-android-codelab/index.html#4

---
+++ app/src/main/java/it/unibs/cloudondemand/oauth/FitbitAuth.java
@@ -0,0 +1,15 @@
+package it.unibs.cloudondemand.oauth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import it.unibs.cloudondemand.R;
+
+public class FitbitAuth extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fitbit_auth);
+    }
+}
---
+++ app/src/main/java/it/unibs/cloudondemand/oauth/RedirectUriReceiver.java
@@ -0,0 +1,12 @@
+package it.unibs.cloudondemand.oauth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RedirectUriReceiver extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
