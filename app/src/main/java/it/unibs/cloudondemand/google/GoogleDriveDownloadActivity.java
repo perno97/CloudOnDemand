a0404d1d2674f90688ed65a6cbb5cd843bd1da15
perno97 <fabio.perini97@gmail.com>
2017-09-26 11:11:06 +0200

    Spostata riga che causava errore.

---
+++ app/src/main/java/it/unibs/cloudondemand/google/GoogleDriveDownloadActivity.java
@@ -0,0 +1,15 @@
+package it.unibs.cloudondemand.google;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import it.unibs.cloudondemand.R;
+
+public class GoogleDriveDownloadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_google_drive_download);
+    }
+}
